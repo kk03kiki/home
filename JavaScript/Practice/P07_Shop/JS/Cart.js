f85e58c4209dd7fab507983953766474eb9b9958
kk03kiki <0037.kasagi.yuka@gmail.com>
2024-06-20 17:10:33 +0900

    6/20

---
+++ JavaScript/Practice/P07_Shop/JS/Cart.js
@@ -0,0 +1,38 @@
+// 買い物カゴClass
+export default class ShoppingBag{
+
+    // クラスプロパティ
+    // ゲットはClass目線（テキスト）
+    // 商品リスト(配列)
+    static #itemLists = []
+
+
+    // 
+    constructor(itemList= []){
+      this.#itemList = itemList
+    }
+
+
+
+    // ここからソッド
+    // 商品追加メソッド
+    // →　index.htmlの「商品追加ボタン」を押したら、「商品追加メソッド」動く。商品購入画面で表示する
+    // プロパティからデータを読み取る時は、getを付ける。
+    // プロパティに値を追加する場合は、setは、プロパティ
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+// メソッド:商品一覧取得
+// メソッド:商品購入
---
+++ JavaScript/Practice/P07_Shop/JS/index.js
@@ -0,0 +1,36 @@
+
+
+// jsonデータより出力
+
+let itemDetail = document.getElementById('itemDetails')
+
+
+window.onload = function () {
+    // データの取得（非同期通信
+    fetch('../data/data.json')
+        // 取得したデータをjson形式に変換
+        .then(response => response.json())
+        // 変換されたデータを表示
+        .then(data => {
+            // data.jasonファイルを「data」とする。data.jasonファイルの「items」を指定
+            // itemsのデータそれぞれを出す（foreachで）
+            // 「item」は、とりあえず「items」個々のデータを定義する
+            // foreach（item 配列の要素（配列全体のこと）,
+            // index 現在の要素の添え字index番号（配列の中の何番目かを定数indexとして表している））、
+            // ここで初めて変数を決める
+            data.items.forEach((item, index) =>{
+                let outHtml = `
+                <table onclick="window.location.href = 'detail.html?index=${index}'">
+                    <tr>
+                        <td>${item.name}</td>
+                        <td>${item.price}円</td>
+                        <td><img src="../img/${item.img}"</td>
+                    </tr>
+                </table>
+                `
+                itemDetails.innerHTML += outHtml
+
+
+            })
+        })
+}
